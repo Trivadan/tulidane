d338e8d5264131d9d3a4aa19bd5613c0aae0a9f4
Danstan <danstanochieng@gmail.com>
2022-12-07 18:07:34 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+(function($) {
+    "use strict";
+
+if ($.fn.classyNav) {
+    $('#roamingnav').classyNav();
+}
+
+// nav scroll   
+var myoffset = $('#dtr-header-global').height();
+$('.navbar a[href^="#"]').click(function () {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - myoffset
+    }, "slow", "easeInSine");
+});
+
+//stickyatTop
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > 670) {
+        $(".scrollheader").addClass("is-sticky");
+        $('.scrollheader').css('position', 'fixed');
+    } else {
+        $(".scrollheader").removeClass("is-sticky");
+        $(".scrollheader").css('position', 'relative');
+    }
+});
+
+    $(document).ready(function() {
+      $('.infiniteslider').owlCarousel({
+        stagePadding:200,
+        center: true,
+        items:2,
+        autoplay:true,
+        loop:true,
+        margin:20,
+        smartSpeed:800,
+        autoplayHoverPause:true,
+        nav:false,
+        dots:false,
+        navText: ['<i class="icofont-arrow-left"></i>', 
+                  '<i class="icofont-arrow-right"></i>'],
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:10,
+                dots:true,
+            },
+            576: {
+                items:2,
+                stagePadding:0,
+            },
+            768: {
+                items:2,
+            },
+            992: {
+                items:3,
+            },
+            1200: {
+                items: 3,
+            }
+        }
+      })
+
+      $('#teamslider').owlCarousel({
+        stagePadding:5,
+        center: true,
+        items:1,
+        autoplay:true,
+        loop:true,
+        margin:10,
+        smartSpeed:1500,
+        autoplayHoverPause:true,
+        nav:false,
+        dots:false,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding:10,
+                dots:true,
+            },
+            576: {
+                items:2,
+                stagePadding:0,
+            }
+        }
+      })
+    });
+
+    //LightBox / Fancybox
+    $('.lightbox-image').fancybox({
+        openEffect: 'fade',
+        closeEffect: 'fade',
+        helpers: {
+            media: {}
+        }
+    });
+
+})(window.jQuery);
+
